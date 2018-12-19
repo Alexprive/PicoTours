485ffe3b38d796132362f0137b39b22d4e285c12
Sven Wilpstra <s.a.wilpstra@gmail.com>
2018-12-19 12:07:36 +0100

    Tour template, changes to footer, etc.

--- resources/views/inc/featuredguides.blade.php
+++ resources/views/inc/featuredguides.blade.php
@@ -3,53 +3,89 @@
 
     <div id="featuredguides_container">
          <div class="styled-heading"><h3>
-               Featured guides</h3>
+               Featured guides</h3><br>
        </div>
-    <div class="card-deck">
-         <div class="card">
-               <img class="card-img-top" src="img/portrait1.jpeg" alt="Card image cap">
-               <div class="card-body">
-               <h5 class="card-title">Helena</h5>
-               <p class="card-text">Tourguide since 2017, experienced in the local cuisine and art scene.</p>
-               </div>
-               <div style="align-self:center">
-                     <a class="btn btn-info" href="/pages/guideprofile/7">Visit Profile <i class="fas fa-angle-double-right"></i></a>
-               </div><br>
-               </div>
 
-               <div class="card">
-               <img class="card-img-top" src="img/portrait2.jpeg" alt="Card image cap">
-               <div class="card-body">
-               <h5 class="card-title">Maria</h5>
-               <p class="card-text">After getting my fashion degree I like to combine my passion for fashion with showing visitors around.</p>
-               </div>
-               <div style="align-self:center">
-                     <a class="btn btn-info" href="/pages/guideprofile/6">Visit Profile <i class="fas fa-angle-double-right"></i></a>
-               </div><br>
-               </div>
-               
-               <div class="card">
-               <img class="card-img-top" src="img/portrait3.jpeg" alt="Card image cap">
-               <div class="card-body">
-               <h5 class="card-title">Alex</h5>
-               <p class="card-text">As a party animal I love to take visitors of my city out for a club crawl. Can you handle it?</p>
-               </div>
-               <div style="align-self:center">
-                     <a class="btn btn-info" href="/pages/guideprofile/1">Visit Profile <i class="fas fa-angle-double-right"></i></a>
-               </div><br>
-               </div>
-               <div class="card">
-                     <img class="card-img-top" src="img/portrait4.jpeg" alt="Card image cap">
-                     <div class="card-body">
-                     <h5 class="card-title">Matthew</h5>
-                     <p class="card-text">Everybody loves Amsterdam, but experiencing the city as a local is an unique experience that only few have had. </p>
-                     </div>
-                     <div style="align-self:center">
-                           <a class="btn btn-info" href="/pages/guideprofile/2">Visit Profile <i class="fas fa-angle-double-right"></i></a>
-                     </div><br>
-                     </div>
-               </div>
-            </div>
-            <div class="form-group" style="text-align: center">
-                  <br> <input type="submit" name="btnSubmit" class="btnContact" value="View All Guides" style="width:150px"/>
-               </div>
+       <div class="container">
+
+                      <div id="products" class="row view-group">
+                          <div class="item col-xs-3 col-lg-3">
+                              <div class="thumbnail card">
+                                    <div class="img-event" style="text-align: center">
+                                      <img style="width:75%; height:100px;" class="group list-group-image img-fluid"
+                                           src="img/portrait1.jpeg" style=>
+                                    </div>
+                                    <div class="caption card-body">
+                                      <h5 class="group card-title inner list-group-item-heading">
+                                                Helena</h5>
+                                      <p class="group inner list-group-item-text">
+                                                Tourguide since 2017, experienced in the local cuisine and art scene.</p>
+                                    <div class="row">
+                                    <div class="col-xs-12 col-md-12" style="text-align:center">
+                                              <a class="btn btn-info" href="/pages/guideprofile/1">View Guide Profile</a>
+                                    </div>
+                                    </div>
+                                  </div>
+                              </div>
+                          </div>
+                      
+                              <div class="item col-xs-3 col-lg-3">
+                                  <div class="thumbnail card">
+                                      <div class="img-event" style="text-align: center">
+                                          <img style="width:75%; height:100px;" class="group list-group-image img-fluid"
+                                               src="img/portrait2.jpeg">
+                                      </div>
+                                      <div class="caption card-body">
+                                          <h5 class="group card-title inner list-group-item-heading">
+                                                    Helena</h5>
+                                          <p class="group inner list-group-item-text">
+                                                    Tourguide since 2017, experienced in the local cuisine and art scene.</p>
+                                          <div class="row">
+                                              <div class="col-xs-12 col-md-12" style="text-align:center">
+                                                  <a class="btn btn-info" href="/pages/guideprofile/2">View Guide Profile</a>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                                    <div class="item col-xs-3 col-lg-3">
+                                        <div class="thumbnail card">
+                                            <div class="img-event" style="text-align: center">
+                                                <img style="width:75%; height:100px;" class="group list-group-image img-fluid"
+                                                     src="img/portrait3.jpeg">
+                                            </div>
+                                            <div class="caption card-body">
+                                                <h5 class="group card-title inner list-group-item-heading">
+                                                          Helena</h5>
+                                                <p class="group inner list-group-item-text">
+                                                          Tourguide since 2017, experienced in the local cuisine and art scene.</p>
+                                                <div class="row">
+                                                    <div class="col-xs-12 col-md-12" style="text-align:center">
+                                                        <a class="btn btn-info" href="/pages/guideprofile/3">View Guide Profile</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                          <div class="item col-xs-3 col-lg-3">
+                                              <div class="thumbnail card">
+                                                  <div class="img-event" style="text-align: center">
+                                                      <img style="width:75%; height:100px;" class="group list-group-image img-fluid"
+                                                           src="img/portrait4.jpeg">
+                                                  </div>
+                                                  <div class="caption card-body">
+                                                      <h5 class="group card-title inner list-group-item-heading">
+                                                                Helena</h5>
+                                                      <p class="group inner list-group-item-text">
+                                                                Tourguide since 2017, experienced in the local cuisine and art scene.</p>
+                                                      <div class="row">
+                                                          <div class="col-xs-12 col-md-12" style="text-align:center">
+                                                              <a class="btn btn-info" href="/pages/guideprofile/4">View Guide Profile</a>
+                                                          </div>
+                                                      </div>
+                                                  </div>
+                                              </div>
+                                          </div>
+                                      
+
+                                        </div>
--- resources/views/pages/tourtemplate.blade.php
+++ resources/views/pages/tourtemplate.blade.php
@@ -12,9 +12,8 @@
     <div class="container">
 
         <div class="styled-heading">
-            <h3>Find your sustainable city tour!</h3>
-            Our {{ $guidecount }} guides offer {{ $tourcount }} tours in {{ $citycount }} cities. Find tours the city
-            you want to visit.
+            <h3>Find all tours of {{ $user->name }}!</h3>
+            Check out all tours of the guide of your choice!
         </div>
 
         <div class="row">
@@ -52,12 +51,12 @@
                 <div class="row">
                     <div class="col-md-12" style="text-align:center">
                         <div class="styled-heading">
-                            <h3>All City Tours</h3>
+                            <h3>All Tours by {{ $user->name }}</h3>
                         </div>
-                        <p>Check out tours at your next destination! </p>
+                        <p>Click a tour for more details or view the profile of the tourguide. </p>
                         <!-- buttons will get other names but use classes for style purposes only now -->
                         <div class="form-group">
-                            <br> <input type="submit" name="btnSubmit" class="btnContact" value="Become a Guide"/>
+                            <br> <input type="submit" name="btnSubmit" class="btnContact" value="View Guide Profile"/>
                         </div>
                     </div>
                 </div>
@@ -66,22 +65,21 @@
                 <!-- Card container with foreach, final working version -->
                 <div class="container">
                     <div class="row pt-4">
-                        @foreach($cities as $city)
+                        @foreach($tours as $tour)
                             <div class="col-lg-4 col-sm-6 py-2">
 
                                 <!-- card -->
                                 <div class="card h-100">
                                     <div class="card-img-top">
                                         <img style="width:100%; height:100px;"
-                                             src="/storage/city_images/{{$city->city_image}}"
+                                             src="/storage/tour_images/{{$tour->tour_image}}"
                                              class="img-fluid mx-auto d-block" alt="card image 1">
                                     </div>
                                     <div class="card-block d-flex flex-column">
-                                        <h5 class="card-title">{{ $city->name }}</h5>
-                                        <p class="text-justify">{!! $city->description !!} </p>
+                                        <h5 class="card-title">{{ $tour->name }}</h5>
+                                        <p class="text-justify">{!! $tour->description !!} </p>
                                         <div class="text-center mt-auto pt-3">
-                                            <a class="btn btn-info" href="/pages/citytemplate/{{$city->id}}">Read
-                                                more <i class="fas fa-angle-double-right"></i></a></div>
+                                            <a class="btn btn-info" href="/pages/tourtemplate/{{$tour->id}}">View Tour Details<i class="fas fa-angle-double-right"></i></a></div>
                                         <br>
                                     </div>
                                 </div>
